fc80537c4dc12d1653623cebf052c9835d1a7a7b
Hikaru Nakamura <hikaru.0601.wed@gmail.com>
2018-03-20 22:47:50 +0900

    add kadai 1
    add kadai 1

--- 15_05_kadai/src/ColLesson01.java
+++ 15_05_kadai/src/ColLesson01.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
 
 public class ColLesson01 {
-	public static void main(String[] args) {
-		// Wordクラスのインスタンス配列
-		Word[] words = new Word[10];
+    public static void main(String[] args) {
+        // Wordクラスのインスタンス配列
+        Word[] words = new Word[10];
 
-		// コマンドラインから入力
-		System.out.println("わからない単語とその意味をスペースで区切って入力して下さい。");
-		Scanner sc  = new Scanner(System.in);
-		String input = sc.nextLine();
+        // コマンドラインから入力
+        System.out.println("わからない単語とその意味をスペースで区切って入力して下さい。");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
-		// ここから記述してください
-		// 例えば、「Dukeくん Javaのマスコットキャラクター」と入力されたときはtmp[0]に"Dukeくん"、tmp[1]に"Javaのマスコットキャラクター"が入る
-		tmp = input.split(" ");
+        // ここから記述してください
+        // 例えば、「Dukeくん Javaのマスコットキャラクター」と入力されたときはtmp[0]に"Dukeくん"、tmp[1]に"Javaのマスコットキャラクター"が入る
+        String[] tmp = input.split(" ", 0);
+        words[0] = new Word(tmp[0], tmp[1]);
 
-	}
+        int COUNT = 1;
+        do {
+            int i = COUNT;
+            System.out.println("次の単語と意味を入力して下さい。\"e\"で終了します。");
+            input = sc.nextLine();
+            if ("e".equals(input)) {
+                break;
+            }
+            tmp = input.split(" ", 0);
+            words[i] = new Word(tmp[0], tmp[1]);
+            COUNT = ++i;
+        } while (COUNT <= 9);
+        sc.close();
+
+
+        for (int i = 0; i < COUNT; i++) {
+            System.out.println("単語；" + words[i].getWord() + " 意味；" + words[i].getMeaning());
+        }
+        System.out.println(COUNT + "件、登録しました。");
+    }
 }

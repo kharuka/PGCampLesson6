5e7e0d5f5a76bdccbbe41ccaf67038f003b6a10f
Hikaru Nakamura <hikaru.0601.wed@gmail.com>
2018-03-21 21:25:59 +0900

    fix kadai 4
    fixed code is using word class

--- 15_05_kadai/src/ColLesson04.java
+++ 15_05_kadai/src/ColLesson04.java
@@ -6,7 +6,7 @@ public class ColLesson04 {
         int NUMBER = 5;
 
         // ArrayList
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<Word> arrayList = new ArrayList<Word>();
 
         // コマンドラインから入力
         System.out.println("わからない単語とその意味をスペースで区切って入力して下さい。");
@@ -16,9 +16,8 @@ public class ColLesson04 {
         // ここから記述してください
         // 例えば、「Dukeくん Javaのマスコットキャラクター」と入力されたときはtmp[0]に"Dukeくん"、tmp[1]に"Javaのマスコットキャラクター"が入る
         String[] tmp = input.split(" ", 0);
-        for (int i = 0; i < 2; i++) {
-            arrayList.add(tmp[i]);
-        }
+        Word words = new Word(tmp[0], tmp[1]);
+        arrayList.add(words);
 
         int COUNT = 1;
         do {
@@ -29,9 +28,8 @@ public class ColLesson04 {
                 break;
             }
             tmp = input.split(" ", 0);
-            for (int j = 0; j < 2; j++) {
-                arrayList.add(tmp[j]);
-            }
+            words = new Word(tmp[0], tmp[1]);
+            arrayList.add(words);
             COUNT = ++i;
         } while (COUNT <= NUMBER - 1);
         sc.close();
@@ -40,8 +38,8 @@ public class ColLesson04 {
             System.out.println("登録制限を超えました。登録済みのデータは以下になります。");
         }
 
-        for (int i = 0; i < arrayList.size(); i += 2) {
-            System.out.println("単語；" + arrayList.get(i) + " 意味；" + arrayList.get(i + 1));
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i).toString());
         }
         System.out.println(COUNT + "件、登録しました。");
     }
